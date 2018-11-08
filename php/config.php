01a7e0fffe5904d50fe7e7a59d01dccd7a0dd8f4
Bernardo Escoffié Torre <it@equilatero.xyz>
2018-11-08 14:49:39 -0600

    Terminado v 0.1

--- php/config.php
+++ php/config.php
@@ -9,5 +9,12 @@ $twig = new Twig_Environment( $loader, []);
 $general = [
     'brand' => 'Pizzas Plaza',
     'title' => 'Pizzas Plaza',
-    'phonePrefix' => '+52 1 55'
+    'phonePrefix' => '52 1 55',
+    'years' => (date('Y')-1978),
+    'thisYear' => date('Y'),
+    'domain' => 'http://docker.local/'
 ];
+
+$menu = json_decode(file_get_contents('menu.json'), true);
+
+$sucursales = json_decode(file_get_contents('sucursales.json'), true);
--- php/xochimilco.php
+++ php/xochimilco.php
@@ -2,21 +2,6 @@
 
 require_once 'config.php';
 
-$sucursal = [
-    'name' => 'Xochimilco',
-    'whatsapp' => '5219991639683',
-    'telefonos' => [
-        '5552 5256',
-        '5653 7701',
-        '5653 7715',
-        '5653 7788',
-    ],
-    'direccion' => 'Tierra y Libertad 69
-    Entrada por Prol. Div. del Norte. Salida por Tierra y Libertad
-    San Lorenzo La Cebada
-    Delegación Xochimilco
-    México CDMX',
-    'gmaps' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.1158232633784!2d-99.124390284614!3d19.277328750621493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce016ce631b757%3A0x8e5aa7c8089180cc!2sPizzas+Plaza!5e0!3m2!1ses!2smx!4v1541622132304'
-];
+$sucursal = $sucursales[0];
 
-echo $twig->render('home.html', compact('general', 'sucursal'));
+echo $twig->render('home.html', compact('general', 'sucursal', 'menu', 'sucursales'));
